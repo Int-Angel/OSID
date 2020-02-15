096fb9bf1d0f7f85ecd5852a2c523c8eba6a4d5f
Ricardo <ricardoglr664@gmail.com>
2020-02-15 14:02:20 -0600

    informacion_personal

---
+++ OSIDAndroid/app/src/main/java/com/example/osid/informacion_personal.java
@@ -0,0 +1,14 @@
+package com.example.osid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class informacion_personal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infomacion_personal);
+    }
+}
