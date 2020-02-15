904dd480e6ae10c9010ee18c23a067197161795e
Ricardo <ricardoglr664@gmail.com>
2020-02-15 17:27:11 -0600

    ..

--- OSIDAndroid/app/src/main/java/com/example/osid/informacion_personal.java
+++ OSIDAndroid/app/src/main/java/com/example/osid/informacion_personal.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
