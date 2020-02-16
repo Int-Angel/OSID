bd94ecc66fda17d8de37c6501cf17cc5bb88f8aa
Ricardo <ricardoglr664@gmail.com>
2020-02-15 23:24:38 -0600

    Update FunctionsActivity.java

--- OSIDAndroid/app/src/main/java/com/example/osid/FunctionsActivity.java
+++ OSIDAndroid/app/src/main/java/com/example/osid/FunctionsActivity.java
@@ -1,16 +1,36 @@
 package com.example.osid;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.osid.DB.DBCONTROLLER;
+import com.example.osid.GLOBAL.GLOBAL;
 import com.example.osid.R;
+
+import org.w3c.dom.Text;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FunctionsActivity extends AppCompatActivity {
+
+    DBCONTROLLER dbcontroller;
+    TextView basal1;
+    TextView basal2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_functions);
+
+        basal1 = findViewById(R.id.basal1_function_ID);
+        basal2 = findViewById(R.id.basal2_functions_ID);
+        Initialitation();
+    }
+
+    void Initialitation(){
+
+        basal1.setText(GLOBAL.user.getBasal() + " U");
+
     }
 }
