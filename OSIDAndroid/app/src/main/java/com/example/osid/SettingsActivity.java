bef9fbcaa31d03a5c072c3bbcd9f536880ce7ae7
CarloLj <carlouxljlj@gmail.com>
2020-02-19 16:00:31 -0600

    Version Actualizada
    empezandp bluettooth pruebas

--- OSIDAndroid/app/src/main/java/com/example/osid/SettingsActivity.java
+++ OSIDAndroid/app/src/main/java/com/example/osid/SettingsActivity.java
@@ -1,18 +1,14 @@
 package com.example.osid;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
@@ -24,18 +20,14 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Set;
-import java.util.UUID;
 
 public class SettingsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     private static final int REQUEST_ENABLE_BT = 0;
     //private static final int REQUEST_DISCOVER_BT = 1;
-    Handler bluetoothIn;
+
     TextView mStatusBlueTv, mPairedTv;
     ImageView mBlueIv;
     Switch onOff;
@@ -43,15 +35,6 @@ public class SettingsActivity extends AppCompatActivity implements CompoundButto
     ListView mListView;
     public ArrayList<BluetoothDevice> mBTdevices = new ArrayList<>();
     BluetoothAdapter mBlueAdapter;
-    final int handlerState = 0;
-    private BluetoothAdapter btAdapter = null;
-    private BluetoothSocket btSocket = null;
-    private StringBuilder DataStringIN = new StringBuilder();
-    private ConnectedThread MyConexionBT;
-    // Identificador unico de servicio - SPP UUID
-    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    // String para la direccion MAC
-    private static String address = null;
     public DeviceListAdapter deviceListAdapter;
 
     private BroadcastReceiver mBroadcastReceiver2 = new BroadcastReceiver() {
@@ -68,27 +51,11 @@ public class SettingsActivity extends AppCompatActivity implements CompoundButto
             }
         }
     };
-=======
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SettingsActivity extends AppCompatActivity {
->>>>>>> parent of a0d00fa... AVANCES GLUCOMETRO
-=======
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SettingsActivity extends AppCompatActivity {
->>>>>>> parent of a0d00fa... AVANCES GLUCOMETRO
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-<<<<<<< HEAD
-<<<<<<< HEAD
         onOff = findViewById(R.id.turnonoff);
         onOff.setOnCheckedChangeListener(this);
         mStatusBlueTv = findViewById(R.id.statusBluetoothTv);
@@ -122,22 +89,6 @@ public class SettingsActivity extends AppCompatActivity {
             onOff.setChecked(false);
         }
 
-        bluetoothIn = new Handler() {
-            public void handleMessage(android.os.Message msg) {
-                if (msg.what == handlerState) {
-                    String readMessage = (String) msg.obj;
-                    DataStringIN.append(readMessage);
-
-                    int endOfLineIndex = DataStringIN.indexOf("#");
-
-                    if (endOfLineIndex > 0) {
-                        String dataInPrint = DataStringIN.substring(0, endOfLineIndex);
-                        IdBufferIn.setText("Dato: " + dataInPrint);//<-<- PARTE A MODIFICAR >->->
-                        DataStringIN.delete(0, DataStringIN.length());
-                    }
-                }
-            }
-        }
 /*
         //on btn click
         mOnBtn.setOnClickListener(new View.OnClickListener() {
@@ -244,13 +195,13 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         showToast("onDestroy: called");
         super.onDestroy();
         //mBluetoothAdapter.cancelDiscovery();
     }
 
-    private void checkBTConnections(){
+    private void checkBTConnections() {
         if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
             int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
             permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
@@ -276,110 +227,4 @@ public class SettingsActivity extends AppCompatActivity {
             mBlueIv.setImageResource(R.drawable.ic_action_off);
         }
     }
-
-    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException
-    {
-        //crea un conexion de salida segura para el dispositivo
-        //usando el servicio UUID
-        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        //Consigue la direccion MAC desde DeviceListActivity via intent
-        Intent intent = getIntent();
-        //Consigue la direccion MAC desde DeviceListActivity via EXTRA
-        address = intent.getStringExtra(DispositivosBT.EXTRA_DEVICE_ADDRESS);//<-<- PARTE A MODIFICAR >->->
-        //Setea la direccion MAC
-        BluetoothDevice device = btAdapter.getRemoteDevice(address);
-
-        try
-        {
-            btSocket = createBluetoothSocket(device);
-        } catch (IOException e) {
-            Toast.makeText(getBaseContext(), "La creacción del Socket fallo", Toast.LENGTH_LONG).show();
-        }
-        // Establece la conexión con el socket Bluetooth.
-        try
-        {
-            btSocket.connect();
-        } catch (IOException e) {
-            try {
-                btSocket.close();
-            } catch (IOException e2) {}
-        }
-        MyConexionBT = new ConnectedThread(btSocket);
-        MyConexionBT.start();
-    }
-
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        try
-        { // Cuando se sale de la aplicación esta parte permite
-            // que no se deje abierto el socket
-            btSocket.close();
-        } catch (IOException e2) {}
-    }
-
-    private class ConnectedThread extends Thread
-    {
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-        public ConnectedThread(BluetoothSocket socket)
-        {
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-            try
-            {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) { }
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run()
-        {
-            byte[] buffer = new byte[256];
-            int bytes;
-
-            // Se mantiene en modo escucha para determinar el ingreso de datos
-            while (true) {
-                try {
-                    bytes = mmInStream.read(buffer);
-                    String readMessage = new String(buffer, 0, bytes);
-                    // Envia los datos obtenidos hacia el evento via handler
-                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
-                } catch (IOException e) {
-                    break;
-                }
-            }
-        }
-        //Envio de trama
-        public void write(String input)
-        {
-            try {
-                mmOutStream.write(input.getBytes());
-            }
-            catch (IOException e)
-            {
-                //si no es posible enviar datos se cierra la conexión
-                Toast.makeText(getBaseContext(), "La Conexión fallo", Toast.LENGTH_LONG).show();
-                finish();
-            }
-        }
-    }
 }
-=======
-    }
-}
->>>>>>> parent of a0d00fa... AVANCES GLUCOMETRO
-=======
-    }
-}
->>>>>>> parent of a0d00fa... AVANCES GLUCOMETRO
