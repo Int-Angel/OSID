616ae95fee6ef652a5bebb6f14a2155cc8fad925
Angel <padilla.angel88@gmail.com>
2020-02-19 19:14:18 -0600

    charts

--- OSIDAndroid/app/src/main/java/com/example/osid/GraphActivity.java
+++ OSIDAndroid/app/src/main/java/com/example/osid/GraphActivity.java
@@ -2,6 +2,7 @@ package com.example.osid;
 
 import android.os.Bundle;
 import android.text.format.DateUtils;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,8 +24,10 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
@@ -34,7 +37,7 @@ public class GraphActivity extends AppCompatActivity {
     DBCONTROLLER dbcontroller;
     Button btn_Hoy, btn_Semana, btn_Mes;
     TextView fechaInicio, fechaFin;
-    TextView testFecha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,103 +48,36 @@ public class GraphActivity extends AppCompatActivity {
 
         fechaInicio = findViewById(R.id.fechaInicio);
         fechaFin = findViewById(R.id.fechaFin);
-        testFecha = findViewById(R.id.testFecha);
 
         btn_Hoy = findViewById(R.id.btn_hoy);
         btn_Mes = findViewById(R.id.btn_mes);
         btn_Semana = findViewById(R.id.btn_semana);
+
+        btn_Hoy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowTodayData();
+            }
+        });
+
+        btn_Semana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowWeekData();
+            }
+        });
+
+        btn_Mes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowMonthData();
+            }
+        });
+
+        //TODO Borrar
         TestData();
         Initialization();
-        //Test();
     }
-
-    void Initialization(){
-
-        Date fechaActual = new Date();
-        fechaInicio.setText(DateToString(fechaActual, "dd/MM/YYYY"));
-        fechaFin.setText(DateToString(fechaActual, "dd/MM/YYYY"));
-
-        //ChartCalculator(StringToDate("2020-02-18 0:00",GLOBAL.DATE_FORMAT),StringToDate("2020-02-18 23:00",GLOBAL.DATE_FORMAT));
-
-        ChartCalculator(fechaActual,fechaActual);
-    }
-
-    void Test(){
-        // TODO poner rangos: ver los valores del dia, o del mes o de un rango especifico
-        //
-        List<Entry> entries = new ArrayList<Entry>();
-        List<Entry> entries2 = new ArrayList<Entry>();
-        String dtStart;
-        for(int i = 1; i <= 30;i++){
-            dtStart = "2020-03-" + i +" 17:00:00";
-            dbcontroller.InsertInsuline(new Insuline(i,StringToDate(dtStart, GLOBAL.DATE_FORMAT),new Random().nextInt((40 - 1) + 1) + 1));
-            dbcontroller.InsertGlucose(new Glucose(i,StringToDate(dtStart, GLOBAL.DATE_FORMAT),new Random().nextInt((300 - 55) + 1) + 55));
-            dtStart = "2020-03-" + i +" 11:00:00";
-            dbcontroller.InsertGlucose(new Glucose(i,StringToDate(dtStart, GLOBAL.DATE_FORMAT),new Random().nextInt((300 - 55) + 1) + 55));
-            dtStart = "2020-03-" + i +" 21:00:00";
-            dbcontroller.InsertGlucose(new Glucose(i,StringToDate(dtStart, GLOBAL.DATE_FORMAT),new Random().nextInt((300 - 55) + 1) + 55));
-        }
-
-        List<Insuline> insulines = new ArrayList<Insuline>(dbcontroller.GetArrayInsuline());
-        List<Glucose> glucoses = new ArrayList<Glucose>(dbcontroller.GetArrayGlucose());
-        Collections.sort(insulines, new Comparator<Insuline>() {
-            @Override
-            public int compare(Insuline insuline, Insuline t1) {
-                return insuline.getFecha().compareTo(t1.getFecha());
-            }
-        });
-        Collections.sort(glucoses, new Comparator<Glucose>() {
-            @Override
-            public int compare(Glucose glucose, Glucose t1) {
-                return glucose.getFecha().compareTo(t1.getFecha());
-            }
-        });
-
-        for(Insuline dataSet : insulines){
-            entries.add(new Entry((long)dataSet.getFecha().getTime(),dataSet.getInsuline()));
-        }
-        for(Glucose dataSet: glucoses){
-            entries2.add(new Entry(dataSet.getFecha().getTime(),dataSet.getGlucose()));
-        }
-
-        ArrayList<LineDataSet> lines = new ArrayList<LineDataSet> ();
-
-        LineDataSet dataSet = new LineDataSet(entries,"Insulina");
-        dataSet.setColor(getColor(R.color.blue));
-        dataSet.setValueTextColor(getColor(R.color.black));
-        dataSet.setFillAlpha(100);
-        dataSet.setDrawFilled(true);
-        dataSet.setFillColor(getColor(R.color.blue));
-        lines.add(dataSet);
-
-        LineDataSet dataSet2 = new LineDataSet(entries2,"Glucosa");
-        dataSet2.setColor(getColor(R.color.colorAccent));
-        dataSet2.setValueTextColor(getColor(R.color.black));
-        dataSet2.setFillAlpha(100);
-        dataSet2.setDrawFilled(true);
-        dataSet2.setFillColor(getColor(R.color.colorAccent));
-        lines.add(dataSet2);
-
-
-        LineData lineData = new LineData();
-        lineData.addDataSet(dataSet);
-        lineData.addDataSet(dataSet2);
-        lineChart.setData(lineData);
-        lineChart.animateY(1000);
-
-        ValueFormatter valueFormatter = new ValueFormatter() {
-            @Override
-            public String getFormattedValue(float value) {
-                Date date = new Date((long)value);
-                //Specify the format you'd like
-                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd");
-                return sdf.format(date);
-            }
-        };
-        XAxis xAxis = lineChart.getXAxis();
-        xAxis.setValueFormatter(valueFormatter);
-    }
-
     void TestData(){
         String dtStart;
         for(int i = 1; i <= 29;i++){
@@ -153,6 +89,10 @@ public class GraphActivity extends AppCompatActivity {
             dtStart = "2020-02-" + i +" 21:00:00";
             dbcontroller.InsertGlucose(new Glucose(i,StringToDate(dtStart, GLOBAL.DATE_FORMAT),new Random().nextInt((300 - 55) + 1) + 55));
         }
+    }
+
+    void Initialization(){
+       ShowTodayData();
     }
 
     void ChartCalculator(Date startDate, Date endDate){
@@ -178,11 +118,10 @@ public class GraphActivity extends AppCompatActivity {
         });
 
         String dateFormat = "YYYYMMdd";
-        Date startDateFinal = StringToDate(DateToString(startDate,dateFormat),dateFormat);
-        Date endDateFinal = StringToDate(DateToString(endDate,dateFormat),dateFormat);
+
         //List<Integer> removeIndexInsulines = new ArrayList<>();
 
-        List<Insuline> removeInsulines = new ArrayList<>();
+        /*List<Insuline> removeInsulines = new ArrayList<>();
         SimpleDateFormat dateFormat1 = new SimpleDateFormat(dateFormat);
         for(int i = 0; i < insulines.size();i++){
             if(StringToDate(DateToString(insulines.get(i).getFecha(),dateFormat),dateFormat).before(startDateFinal) || StringToDate(DateToString(insulines.get(i).getFecha(),dateFormat),dateFormat).after(endDateFinal)){
@@ -198,7 +137,7 @@ public class GraphActivity extends AppCompatActivity {
         }
 
         //List<Integer> removeIndexGlucoses = new ArrayList<>();
-      /*  List<Glucose> removeGlucose = new ArrayList<>();
+        List<Glucose> removeGlucose = new ArrayList<>();
         for(int i = 0; i < glucoses.size();i++){
             if(glucoses.get(i).getFecha().before(startDateFinal) || glucoses.get(i).getFecha().after(endDateFinal)){
                 removeGlucose.add(glucoses.get(i));
@@ -214,10 +153,14 @@ public class GraphActivity extends AppCompatActivity {
 
 
         for(Insuline dataSet : insulines){
-            insulinaEntries.add(new Entry((long)dataSet.getFecha().getTime(),dataSet.getInsuline()));
+            if(IsBeforeDate(dataSet.getFecha(),endDate) && IsAfterDate(dataSet.getFecha(),startDate)){
+                insulinaEntries.add(new Entry(dataSet.getFecha().getTime(),dataSet.getInsuline()));
+            }
         }
         for(Glucose dataSet: glucoses){
-            glucosaEntries.add(new Entry(dataSet.getFecha().getTime(),dataSet.getGlucose()));
+            if(IsBeforeDate(dataSet.getFecha(),endDate) && IsAfterDate(dataSet.getFecha(),startDate)){
+                glucosaEntries.add(new Entry(dataSet.getFecha().getTime(), dataSet.getGlucose()));
+            }
         }
 
         ArrayList<LineDataSet> lines = new ArrayList<LineDataSet> ();
@@ -242,19 +185,126 @@ public class GraphActivity extends AppCompatActivity {
         lineData.addDataSet(dataSetInsulina);
         lineData.addDataSet(dataSetGlucosa);
         lineChart.setData(lineData);
-        lineChart.animateY(1000);
+        lineChart.animateY(400);
 
         ValueFormatter valueFormatter = new ValueFormatter() {
             @Override
             public String getFormattedValue(float value) {
                 Date date = new Date((long)value);
                 //Specify the format you'd like
-                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd");
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM HH:mm");
                 return sdf.format(date);
             }
         };
         XAxis xAxis = lineChart.getXAxis();
         xAxis.setValueFormatter(valueFormatter);
+        xAxis.setLabelCount(4);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+    }
+
+    void ShowTodayData(){
+        Date fechaActual = new Date();
+
+        fechaInicio.setText(DateToString(fechaActual, "dd/MM/YYYY"));
+        fechaFin.setText(DateToString(fechaActual, "dd/MM/YYYY"));
+
+        ChartCalculator(fechaActual,fechaActual);
+    }
+
+    void ShowWeekData(){
+        Date fechaActual = new Date();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(fechaActual);
+        calendar.add(Calendar.DAY_OF_MONTH, Calendar.SUNDAY - calendar.get(Calendar.DAY_OF_WEEK));
+
+        Date startDate = calendar.getTime();
+
+        calendar.setTime(fechaActual);
+        calendar.add(Calendar.DAY_OF_MONTH, Calendar.SUNDAY +(Calendar.SATURDAY - calendar.get(Calendar.DAY_OF_WEEK) - 1));
+
+        Date endDate = calendar.getTime();
+
+        fechaInicio.setText(DateToString(startDate,"dd/MM/YYYY"));
+        fechaFin.setText(DateToString(endDate,"dd/MM/YYYY"));
+        ChartCalculator(startDate,endDate);
+    }
+
+    void ShowMonthData(){
+        Date fechaActual = new Date();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(fechaActual);
+        //calendar.add(Calendar.DAY_OF_MONTH, Calendar.SUNDAY - calendar.get(Calendar.DAY_OF_WEEK));
+        calendar.set(Calendar.DAY_OF_MONTH,
+                calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
+
+        Date startDate = calendar.getTime();
+
+        calendar.setTime(fechaActual);
+        //calendar.add(Calendar.DAY_OF_MONTH, Calendar.SUNDAY +(Calendar.SATURDAY - calendar.get(Calendar.DAY_OF_WEEK) - 1));
+        calendar.set(Calendar.DAY_OF_MONTH,
+                calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        Date endDate = calendar.getTime();
+
+        fechaInicio.setText(DateToString(startDate,"dd/MM/YYYY"));
+        fechaFin.setText(DateToString(endDate,"dd/MM/YYYY"));
+        ChartCalculator(startDate,endDate);
+    }
+
+    public boolean IsAfterDate(Date date1, Date date2){
+        // si la fecha 1 es despues que la fecha 2 entonces TRUE
+        // FECHA1 > FECHA2 TRUE
+        // FECHA1 < FECHA2 FALSE
+        // FECHA1 == FECHA2 TRUE
+        String format = "dd/MM/yyyy";
+        String strDate1 = DateToString(date1,format);
+        String strDate2 = DateToString(date2,format);
+
+        String[] strDate1Array = strDate1.split("/");
+        String[] strDate2Array = strDate2.split("/");
+
+        if(Integer.parseInt(strDate1Array[2]) >= Integer.parseInt(strDate2Array[2])){
+            if(Integer.parseInt(strDate1Array[1]) >= Integer.parseInt(strDate2Array[1])){
+                if(Integer.parseInt(strDate1Array[0]) >= Integer.parseInt(strDate2Array[0])){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    public boolean IsBeforeDate(Date date1, Date date2){
+        // si la fecha 1 es antes que la fecha 2 entonces FALSE
+        // FECHA1 > FECHA2 FALSE
+        // FECHA1 < FECHA2 TRUE
+        // FECHA1 == FECHA2 TRUE
+        String format = "dd/MM/yyyy";
+        String strDate1 = DateToString(date1,format);
+        String strDate2 = DateToString(date2,format);
+
+        String[] strDate1Array = strDate1.split("/");
+        String[] strDate2Array = strDate2.split("/");
+
+        if(Integer.parseInt(strDate1Array[2]) <= Integer.parseInt(strDate2Array[2])){
+            if(Integer.parseInt(strDate1Array[1]) <= Integer.parseInt(strDate2Array[1])){
+                if(Integer.parseInt(strDate1Array[0]) <= Integer.parseInt(strDate2Array[0])){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }else {
+            return false;
+        }
     }
 
     public static Date StringToDate(String dateStr, String format){
@@ -272,4 +322,5 @@ public class GraphActivity extends AppCompatActivity {
             return null;
         }
     }
+
 }
